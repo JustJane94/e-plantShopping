502e9f2c9a89867e41f64a111df1f08b0bda5943
JustJane94 <ukamakajane101@gmail.com>
2026-03-01 18:55:46 -0500

    Task 6: Set up Global Store using Provider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+// 1. Import the Provider and the Store
+import { Provider } from 'react-redux';
+import store from './store.js';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* 2. Wrap the App component with the Provider */}
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
 )
