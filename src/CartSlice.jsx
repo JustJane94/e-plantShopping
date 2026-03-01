4e331e3af6ca85f6c45ab9fa4716f7a41ed35dc0
JustJane94 <ukamakajane101@gmail.com>
2026-03-01 17:02:12 -0500

    Finalizing Task 5

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,14 +7,30 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
-    },
+        // 1. Unpacking the Box (Destructuring)
+        const { name, image, cost } = action.payload;
+      
+        // 2. The Search
+        const existingItem = state.items.find(item => item.name === name);
+      
+        if (existingItem) {
+          // 3. The "Plus One" logic
+          existingItem.quantity++;
+        } else {
+          // 4. The "New Arrival" logic
+          state.items.push({ name, image, cost, quantity: 1 });
+        }
+      },
     removeItem: (state, action) => {
-    },
+        state.items = state.items.filter(item => item.name !== action.payload);
+      },
     updateQuantity: (state, action) => {
-
-    
-    },
+        const { name, quantity } = action.payload;
+        const itemToUpdate = state.items.find(item => item.name === name);
+        if (itemToUpdate) {
+          itemToUpdate.quantity = quantity;
+        }
+      },
   },
 });
 
